ebe08f2e0b4add273577f0ca053ff83a98d35462
ace ge <m18516777845@163.com>
2019-09-10 23:44:20 +0800

    refactor for merge module

--- blade-service/blade-system/src/main/java/org/springblade/system/SystemApplication.java
+++ blade-service/blade-system/src/main/java/org/springblade/system/SystemApplication.java
@@ -27,8 +27,10 @@ import org.springframework.context.annotation.ComponentScan;
  */
 @SpringCloudApplication
 @EnableFeignClients(AppConstant.BASE_PACKAGES)
-//@ComponentScan
-@ComponentScan(basePackages = {"org.springblade.system.user.controller","org.springblade.system.user.feign","org.springblade.system.user.service.*","org.springblade.system.user.*.*"})
+@ComponentScan(basePackages = {"org.springblade.system.user.controller"})
+@ComponentScan(basePackages = {"org.springblade.system.user.feign"})
+@ComponentScan(basePackages = {"org.springblade.system.user.service.*"})
+@ComponentScan(basePackages = {"org.springblade.system.user.*.*"})
 public class SystemApplication {
 
 	public static void main(String[] args) {
